efb32eb13ec01bd1463392ea5bb66a7bc8207294
jialin wu <jialin.wu.02@gmail.com>
2019-08-09 22:04:16 -0700

    init

--- src/App.js
+++ src/App.js
@@ -4,21 +4,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <div class="typewriter">
+          <h1>Hello, I am Jialin Wu. </h1>
+          {/* <h1>I love web and design. </h1> */}
+        </div>
+      </div>
+
     </div>
   );
 }
