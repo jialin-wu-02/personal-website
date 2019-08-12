ea4c26478d24118d5657950b5ae234de1422f2d4
jialin wu <jialin.wu.02@gmail.com>
2019-08-11 23:18:48 -0700

    Updates

---
+++ precache-manifest.4f7e0dabee4390e10b78b24e069d96b4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ec0ceed650e0ba9d6e3a0a5bc76b84c",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "bbe78cde03f2799a6ead",
+    "url": "/personal-website/static/css/main.5206390e.chunk.css"
+  },
+  {
+    "revision": "aea750a338bb3738a1a1",
+    "url": "/personal-website/static/js/2.f9ea5aad.chunk.js"
+  },
+  {
+    "revision": "bbe78cde03f2799a6ead",
+    "url": "/personal-website/static/js/main.45de1a38.chunk.js"
+  },
+  {
+    "revision": "94fb9e20c0aa47c614f8",
+    "url": "/personal-website/static/js/runtime~main.17b5731e.js"
+  },
+  {
+    "revision": "f6acb8d646bb1f3028efd4399c8b3e35",
+    "url": "/personal-website/static/media/admin.f6acb8d6.png"
+  },
+  {
+    "revision": "17c53a4866fa7c03ba9e7ed20f02546e",
+    "url": "/personal-website/static/media/dress.17c53a48.png"
+  },
+  {
+    "revision": "09606fb12b2ff8f6960ce10a132f13ec",
+    "url": "/personal-website/static/media/library.09606fb1.png"
+  },
+  {
+    "revision": "2087db045603c0a9720674d183438326",
+    "url": "/personal-website/static/media/piano.2087db04.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/personal-website/static/media/react_logo.47ce6e77.png"
+  },
+  {
+    "revision": "0647e7f7b9371b8065d9e24095fc1947",
+    "url": "/personal-website/static/media/weather.0647e7f7.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.0d5ddcc04b09348920032ccede329300.js"
+  "/personal-website/precache-manifest.4f7e0dabee4390e10b78b24e069d96b4.js"
 );
 
 self.addEventListener('message', (event) => {
