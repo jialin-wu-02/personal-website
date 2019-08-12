bf60a1500cddb2be2b425b0580bbc347e9bb3118
jialin wu <jialin.wu.02@gmail.com>
2019-08-11 23:42:39 -0700

    roll back

--- src/Component/Project/Project.js
+++ src/Component/Project/Project.js
@@ -3,14 +3,12 @@ import './Project.css';
 
 import Card from '../Card/Card'
 
-import admin from '../../Asset/img/admin.png'
-import dress from '../../Asset/img/dress.png'
-import library from '../../Asset/img/library.png'
-import weather from '../../Asset/img/weather.png'
-import piano from '../../Asset/img/piano.png'
-import react_logo from '../../Asset/img/react_logo.png'
-
-
+// import admin from '../../Asset/img/admin.png'
+// import dress from '../../Asset/img/dress.png'
+// import library from '../../Asset/img/library.png'
+// import weather from '../../Asset/img/weather.png'
+// import piano from '../../Asset/img/piano.png'
+// import react_logo from '../../Asset/img/react_logo.png'
 
 class Project extends Component {
 
@@ -30,29 +28,29 @@ class Project extends Component {
         </div>
         <div className="row">
           <Card>
-            <img src={admin}/>
+            <img src="/img/admin.png"/>
             <h3> Admin Page</h3>
           </Card>
           <Card>
-            <img src={dress}/>
+            <img src="/img/dress.png" />
              <h3> Maison-Autelier </h3>
           </Card>
           <Card>
-            <img src={library}/>
+            <img src="/img/library.png" />
              <h3> Project-Clio </h3>
           </Card>
         </div>
         <div className="row">
         <Card>
-          <img src={weather}/>
+          <img src="/img/weather.png" />
            <h3> Weather Forecast </h3>
         </Card>
         <Card>
-        <img src={piano}/>
+        <img src="/img/piano.png"  />
            <h3> Pianoo </h3>
         </Card>
         <Card>
-        <img src={react_logo} />
+        <img src="/img/react_logo.png" />
            <h3> React China </h3>
         </Card>
         </div>
